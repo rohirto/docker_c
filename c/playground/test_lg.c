e3d8fe1e9cfc564f94d1a4ab645e3470ab3d918a
rohirto <rohirto@gmail.com>
2023-09-06 08:05:54 +0000

    LG Test code update

--- c/playground/test_lg.c
+++ c/playground/test_lg.c
@@ -10,7 +10,7 @@
 #define ARR_SIZE1   5
 #define ARR_SIZE2   5
 
-
+#if 0
 void print_array(int arr[], int len)
 {
     for(int i = 0; i < len; i++)
@@ -130,3 +130,80 @@ int main()
 
     return 0;
 }
+#endif
+
+
+//Dynamic Programming 
+#if 1
+void swap_code(int arr[], int i, int j)
+{
+    // j > i
+    int temp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp;
+}
+void print_array(int arr[], int len)
+{
+    for (int i = 0; i < len; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+int main() {
+    int n1,n2,n3, i, j, temp;
+
+    printf("Enter the number of elements in the array1: ");
+    scanf("%d", &n1);
+
+    printf("Enter the number of elements in the array2: ");
+    scanf("%d", &n2);
+
+    n3 = n1+n2;
+
+    int arr[n1], arr1[n2], arr2[n3];
+
+    printf("Enter %d elements of arr 1:\n", n1);
+    for (i = 0; i < n1; i++) {
+        scanf("%d", &arr[i]);
+        arr2[i] = arr[i];
+        // Sort the array elements immediately within the loop
+        for (j = 0; j < i; j++) {
+            if (arr[j] > arr[i]) {
+                // Swap elements if they are out of order
+                // temp = arr[j];
+                // arr[j] = arr[i];
+                // arr[i] = temp;
+                // Swap code
+                swap_code(arr, i, j);
+                swap_code(arr2,i,j);
+            }
+        }
+    }
+    printf("Enter %d elements of arr 2:\n", n2);
+    for (i = 0; i < n2; i++) {
+        scanf("%d", &arr1[i]);
+        arr2[n1+i] = arr1[i];
+        // Sort the array elements immediately within the loop
+        for (j = 0; j < i; j++) {
+            if (arr1[j] > arr1[i]) {
+                // Swap elements if they are out of order
+                // temp = arr[j];
+                // arr[j] = arr[i];
+                // arr[i] = temp;
+                // Swap code
+                swap_code(arr1, i, j);
+                swap_code(arr2, n1+i,n1+j);
+            }
+        }
+    }
+
+    printf("Sorted array: ");
+    print_array(arr,n1);
+    print_array(arr1,n2);
+    print_array(arr2,n3);
+    
+
+    return 0;
+}
+
+#endif
