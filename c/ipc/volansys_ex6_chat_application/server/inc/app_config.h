8c7ab84129c5606d3f4b1c8812e87868bc71a9d3
rohirto <rohirto@gmail.com>
2023-11-24 09:56:54 +0000

    Docy Cpmments and USE_DEBUG Feature

--- c/ipc/volansys_ex6_chat_application/server/inc/app_config.h
+++ c/ipc/volansys_ex6_chat_application/server/inc/app_config.h
@@ -13,24 +13,25 @@
 #define APP_CONFIG_H
 
 //Customizations
-#define USE_SELECT      0
-#define USE_SELECT_CLI  1
-#define USE_THREADS     1
+#define USE_SELECT      0   /**< Use Select Functionality in Main thread */
+#define USE_SELECT_CLI  1   /**< Use Select Functionality in Client thread */
+#define USE_THREADS     1   /**< Use Thread Functionality to implement server */
+#define USE_DEBUG       1   /**< Toggle debug messages */
 
 
 //Defines
-#define MAX_USERS       128
-#define PORT "9034" // port we're listening on
-#define MAX_LISTEN_BACKLOG      10
+#define MAX_USERS       128         /**< Max users which can sign up for Server */
+#define PORT "9034"                 /**< Server Port */
+#define MAX_LISTEN_BACKLOG      10  /**< Max backlog for Listen */
 
 #if USE_THREADS
-#define THREAD_POOL_SIZE    20
+#define THREAD_POOL_SIZE    20      /**< Thread Pool Size */
 #endif
 
-#define CONFIG_PACKET       0x01
-#define CHAT_INIT_PACKET    0x02
-#define MESSAGE_PACKET      0x03
-#define ERROR_PACKET        0x04
+#define CONFIG_PACKET       0x01    /**< Config Packets - for config purpose - username getting*/
+#define CHAT_INIT_PACKET    0x02    /**< Chat init packet - from client to server only, to let server know which user Id to chat with */
+#define MESSAGE_PACKET      0x03    /**< Message Packet - Carries message intended for the other client thread */
+#define ERROR_PACKET        0x04    /**< Error packets - from server to client only, when some error has occured */
 
 
 //Static Library declarations
