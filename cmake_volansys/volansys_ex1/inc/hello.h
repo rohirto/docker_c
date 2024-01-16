6b5ded4ff8d414995006ffc1ac2dfa59bcc1e199
rohirto <rohirto@gmail.com>
2024-01-16 07:15:27 +0000

    Volansys Exercise 1

---
+++ cmake_volansys/volansys_ex1/inc/hello.h
@@ -0,0 +1,16 @@
+/**
+ * @file hello.h
+ * @author rohit S
+ * @brief Demo header file for cmake exercise
+ * @version 0.1
+ * @date 2024-01-16
+ * 
+ * @copyright Copyright (c) 2024 Volansys Technologies
+ * 
+ */
+#ifndef HELLO_H
+#define HELLO_H
+
+#define HELLO   "hello world"
+
+#endif
